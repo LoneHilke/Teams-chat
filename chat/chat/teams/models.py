5cbe8c52f04ef56b7a13572a52f29a5a84fb5845
LMH <90649822+LoneHilke@users.noreply.github.com>
2024-03-20 22:09:58 +0100

    Django Add Link to Details
    kommet til ovenstående i W3
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- chat/chat/teams/models.py
+++ chat/chat/teams/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Links (models.Model):
+    Title = models.CharField(max_length=100)
+    Fra_JQ = models.URLField(blank=True)
+    Beskrivelse = models.TextField(blank=True)
+    Sendt = models.DateField()
+    Fra_F_LH = models.URLField(blank=True)
+    Beskriv = models.TextField(blank=True)
+    #Fuldført = models.CheckConstraint(blank=True)
--- chat/chat/teams/views.py
+++ chat/chat/teams/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Links
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    links = Links.objects.all().values()
+    template = loader.get_template('teams/base.html')
+    context = {
+        'links':links
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+#fra: https://www.w3schools.com/django/django_add_link_details.php
